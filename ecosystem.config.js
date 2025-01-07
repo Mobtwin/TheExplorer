8021911d2b40ffda2d409988dfd44db5d176a7c3
Bourichi-Taha <bourichi.taha@gmail.com>
2025-01-07 15:25:42 +0100

    added explorer ecosystem file for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+      {
+        name: "theExplorer", // Name of the application
+        script: "TheExplorer.js", // Path to your main application file
+        instances: "1", // Number of instances (e.g., 1, 2, or 'max' for clustering mode)
+        exec_mode: "cluster", // Use 'cluster' for multiple instances or 'fork' for a single instance
+        watch: true, // Restart app on file changes
+        max_memory_restart: '2G', // Restart the app if memory usage exceeds this value
+      },
+    ],
+  };
+  
